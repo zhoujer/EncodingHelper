a4ebe227c45960db56020adc3f484279f7b8d431
Nate Osher <Nateoshe@gmail.com>
2015-04-05 14:10:39 -0700

    Tentative final draft of EncodingHelperCharTest.java with constructor tests

--- src/edu/carleton/oshern/EncodingHelperCharTest.java
+++ src/edu/carleton/oshern/EncodingHelperCharTest.java
@@ -17,6 +17,23 @@ import static org.junit.matchers.JUnitMatchers.hasItems;
  */
 public class EncodingHelperCharTest {
 
+    @Test
+    public void testIntConstructorShouldWork() throws Exception{
+        EncodingHelperChar thing = new EncodingHelperChar(0x42);
+        org.junit.Assert.assertEquals("Failure- Constructor failed to create instance of EncodingHelperChar - code point constructor", 0x42, thing.getCodePoint());
+    }
+
+    @Test
+    public void testUtf8ConstructorShouldWork() throws Exception{
+        EncodingHelperChar thing = new EncodingHelperChar(new byte[]{(byte) 0xC3,(byte) 0xA9 });
+        org.junit.Assert.assertNotNull("Failure- Constructor failed to create instance of EncodingHelperChar - byte constructor", thing);
+    }
+
+    @Test
+    public void testCharConstructorShouldWork() throws Exception{
+        EncodingHelperChar thing = new EncodingHelperChar('A');
+        org.junit.Assert.assertNotNull("Failure- Constructor failed to create instance of EncodingHelperChar - char constructor", thing);
+    }
 
     @Test
     public void testGetCodePointShouldNotReturnNull() throws Exception {
@@ -25,24 +42,31 @@ public class EncodingHelperCharTest {
     }
 
     @Test
+    public void testGetCodePointShouldReturnCorrectValue() throws Exception{
+        EncodingHelperChar thing = new EncodingHelperChar(0x42);
+        org.junit.Assert.assertEquals("Failure- should return correct value", 0x42, thing.getCodePoint());
+    }
+
+    @Test
     public void testSetCodePointShouldChangeValue() throws Exception {
         EncodingHelperChar thing = new EncodingHelperChar(null);
         thing.setCodePoint(0x42);
-        org.junit.Assert.assertEquals("Failure- should return true", 0x42, thing.getCodePoint());
+        org.junit.Assert.assertEquals("Failure- values should be equal", 0x42, thing.getCodePoint());
     }
 
     @Test
     public void testToUtf8BytesShouldReturnCorrectValues() throws Exception {
         EncodingHelperChar thing = new EncodingHelperChar('A');
-        byte[] expected = new byte[10];
-        expected[0] = 0x41;
+        //byte[] expected = new byte[10];
+        //expected[0] = 0x41;
+        byte[] expected = new byte[]{(byte) 0x41};
         org.junit.Assert.assertArrayEquals("Failure- byte arrays are not the same", expected, thing.toUtf8Bytes());
     }
 
     @Test
     public void testToUtf8BytesShouldNotReturnNull() throws Exception {
         EncodingHelperChar thing = new EncodingHelperChar('A');
-        org.junit.Assert.assertNotNull("Failure- byte array was empty", thing.toUtf8Bytes());
+        org.junit.Assert.assertNotNull("Failure- returned null", thing.toUtf8Bytes());
     }
 
     @Test
